d97bf87106fae0086dc13b796addb4fa63a18acc
Дарья Захарова <dasha05z@yandex.ru>
2024-09-17 21:31:22 +0300

    Its over?

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -2,12 +2,17 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from cats.views import CatViewSet
+from cats.views import CatViewSet, LightCatViewSet, OwnerViewSet
 
 
 router = DefaultRouter()
 router.register('cats', CatViewSet)
+router.register('owners', OwnerViewSet)
+router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('auth/', include('djoser.urls.jwt')),
 ]
